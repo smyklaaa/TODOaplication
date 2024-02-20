74e0249dd642a5cb6c6fa3ade6c3c5575e1838ca
purple_orange_beach <52859277+smyklaaa@users.noreply.github.com>
2024-02-20 13:43:44 +0100

    Initial commit

---
+++ src/main/java/com/example/todoaplication/ToDoAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todoaplication/ToDoAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
